6238fada73fdff73ea454dba70b779ea06cd242b
amisu Rahman <hamisurahman@gmail.com>
2023-02-19 12:03:12 -0800

    ,

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main- Entry point
+ * Description: prints alphabet in lower case
+ * Return: (0)
+ */
+
+int main(void)
+{
+	int ch;
+	for(ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+return (0);
+}
