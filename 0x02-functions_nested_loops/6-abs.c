04e28d8c1702c0d7d7b05e65f00ed2f931f92189
amisu Rahman <hamisurahman@gmail.com>
2023-02-21 14:50:21 -0800

    Task 6 - Absolute integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main"
+
+/**
+ * _abs - Entry point
+ * Description - Print absolute value
+ * @n: Interger of Abs Value
+ * Return: int
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{ 
+		return (n);
+	}
+}
