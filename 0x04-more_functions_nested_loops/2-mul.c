9d0162ede771f25e9ba627b861c5cf70c7cefe48
amisu Rahman <hamisurahman@gmail.com>
2023-02-23 13:47:31 -0800

    2. Collaborationis multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers (a and b)
+ * @a: first number to be multiplied
+ * @b: number to be multiplied against
+ * Return: the product of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,1 +1,3 @@
 0.isupper - Write a function that checks for uppercase character\n
+1.isdigit - Write a function that checks for adigit (0 through 9)
+2- Write a function that multiplies two integers
