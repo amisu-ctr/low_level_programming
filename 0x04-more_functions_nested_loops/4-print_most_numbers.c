13557af83715690611ec45b1e307ed2e47b97879
amisu Rahman <hamisurahman@gmail.com>
2023-02-23 14:21:42 -0800

    Fixe

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,12 +10,12 @@ void print_most_numbers(void)
 {
 	int i;
 
-	for (i = 48; i < 58; i++)
+	for (i == 48; i < 58; i++)
 	{
-		if (!(1 == 50) && !(i == 52))
+		if (!(i == 50) && !(i == 52))
 		{
 			putchar(i);
 		}
-		putchar("\n");
 	}
+	putchar(10);
 }
