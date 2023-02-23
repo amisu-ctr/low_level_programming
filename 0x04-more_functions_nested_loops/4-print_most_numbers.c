097524f3c3bce158a5f9229e661a25bd1718368d
amisu Rahman <hamisurahman@gmail.com>
2023-02-23 14:48:08 -0800

    Edit file

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,14 +8,15 @@ include "main.h"
 
 void print_most_numbers(void)
 {
-int i;
+	int a = 0;
 
-for (i = 48; i < 58; i++)
-{
-if (!(i == 50) && !(i == 52))
-{
-putchar(i);
-}
-}
-putchar(10);
+	while (a <= 9)
+	{
+		if (a != 2 && a != 4)
+		{
+			_putchar(a + '0');
+		}
+		a++;
+	}
+	_putchar('\n');
 }
