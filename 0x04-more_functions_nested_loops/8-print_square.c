50ae2ab2d9064f8a4d21fcb86a78575c882cf7cb
amisu Rahman <hamisurahman@gmail.com>
2023-02-24 16:02:54 -0800

    print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_square - prints a size by size square
+ * @size: an int given by main
+ * Description: Uses headers to ink and a nested loops to achieve goal
+ * Return: void no return
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+		_putchar('\');
+	for (i = size; i > 0; i--)
+	{
+		for (j = size; j > 0; j--)
+		{
+			_putchar('#);
+		}
+		_putchar('\n');
+	}
+}
