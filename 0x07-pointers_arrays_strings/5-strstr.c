3625aefa90c58dd2dea1f172732b3864eea94b74
amisu-ctr <hamisurahman@gmail.com>
2023-03-07 00:06:59 +0000

    Task 5 - find substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,20 +2,19 @@
 
 /**
  * _strstr - Locates a substring.
- * @haystack: the string to be searched.
- * @needle: The substring to be located
+ * @haystack: The string to be searched.
+ * @needle: The substring to be located.
  *
- * Return: If the substring is located - a pointer to the beginning 
- * of the located substring.
- * If the substring is not located - NULL
+ * Return: If the substring is located - a pointer to the beginning
+ *                                       of the located substring.
+ * If the substring is not located - NULL.
  */
-
 
 char *_strstr(char *haystack, char *needle)
 {
 	int index;
 
-	if (*needle === 0)
+	if (*needle == 0)
 		return (haystack);
 
 	while (*haystack)
@@ -26,14 +25,14 @@ char *_strstr(char *haystack, char *needle)
 		{
 			do {
 				if (needle[index + 1] == '\0')
-					return (haystaack);
+					return (haystack);
 
 				index++;
 
 			} while (haystack[index] == needle[index]);
 		}
-		
-		haysack++;
+
+		haystack++;
 	}
 
 	return ('\0');
