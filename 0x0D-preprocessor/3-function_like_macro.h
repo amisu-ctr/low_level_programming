bd7781da7e9f1b17b1a2c2726cb333e48d053a5d
amisu-ctr <hamisurahman@gmail.com>
2023-03-21 17:15:10 +0000

    Function like macro that computes an absolute value

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/*
+ * File: 3-functin_like_macro.h
+ * Desc: Header file that defines a function like macro ABS(x)
+ *     that computes the absolute value of a member x
+ */
+
+#define ABS(x) ((x) < 0 ? (-x) : (x))
+
+#endif
